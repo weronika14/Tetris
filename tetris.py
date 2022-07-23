7771a08df27e66447c7b2753a52e33e8313db98f
weronika14 <109104149+weronika14@users.noreply.github.com>
2022-07-23 10:36:48 +0100

    Add files via upload

---
+++ tetris.py
@@ -0,0 +1,472 @@
+import pygame
+import random
+import math
+
+#i need to sort out the timing and the scoring
+
+
+pygame.init()
+playScreenWidth, playScreenHeight = 400, 800
+screenWidth, screenHeight = 600, 800
+screen = pygame.display.set_mode((screenWidth,screenHeight))
+screen.fill(0)
+white = (255,255,255)
+count = 1
+sideKeyDown = False
+downwardsKey = False
+spaceDown = False
+width = 40
+currentRect = []
+screenRect = []
+clock = pygame.time.Clock()
+for i in range(0,playScreenWidth, width):
+    for j in range(0,playScreenHeight,width):
+        screenRect.append(pygame.Rect([i+0.01,j+0.01], [width-0.1, width-0.1]))
+
+
+class Block:
+
+    def __init__(self):
+
+        number = random.randint(0,6)
+        #number = 6
+        #random.choice polecam
+
+        if number == 0:
+            self.shape = YellowSquare()
+        elif number == 1:
+            self.shape = BlueL()
+        elif number == 2:
+            self.shape = OrangeL()
+        elif number == 3:
+            self.shape = PurpleT()
+        elif number == 4:
+            self.shape = GreenShape()
+        elif number == 5:
+            self.shape = RedShape()
+
+        elif number == 6:
+            self.shape = BlueLine()
+
+        self.colour = self.shape.colour
+
+    def falling(self,direction):
+        for i in range(0,4):
+            self.shape.currentSquares[i].fall(direction, i)
+
+    def drawing(self):
+        #self.shape.draw()
+        pygame.draw.rect(screen, self.colour, currentRect[0])
+        pygame.draw.rect(screen, white, currentRect[0],1)
+        pygame.draw.rect(screen, self.colour, currentRect[1])
+        pygame.draw.rect(screen, white, currentRect[1],1)
+        pygame.draw.rect(screen, self.colour, currentRect[2])
+        pygame.draw.rect(screen, white, currentRect[2],1)
+        pygame.draw.rect(screen, self.colour, currentRect[3])
+        pygame.draw.rect(screen, white, currentRect[3],1)
+        pygame.draw.circle(screen, (255,255,255), self.shape.centre, 5)
+        #rotated = pygame.transform.rotate(screen, self.shape.currentSquares[0])
+
+    def rotate(self):
+        for i in range(0,4):
+            xDistance = currentBlock.shape.currentSquares[i].x - (currentBlock.shape.xCoordinate)
+            yDistance = currentBlock.shape.currentSquares[i].y - (currentBlock.shape.yCoordinate)
+            currentBlock.shape.currentSquares[i].x = yDistance * -1 + currentBlock.shape.xCoordinate - width
+            currentBlock.shape.currentSquares[i].y = xDistance + currentBlock.shape.yCoordinate
+
+        for i in range(0,4):
+            currentBlock.shape.currentSquares[i].updating(i)
+
+        allShapes.collision()
+        if allShapes.collided:
+            allShapes.collided = False
+            return 'collided'
+        for i in range(0,4):
+            if currentRect[i].x < 0 or currentRect[i].x >= playScreenWidth:
+                return 'collided'
+
+    def rotateClockwise(self):
+        for i in range(0,4):
+            xDistance = currentBlock.shape.currentSquares[i].x - (currentBlock.shape.xCoordinate)
+            yDistance = currentBlock.shape.currentSquares[i].y - (currentBlock.shape.yCoordinate)
+            currentBlock.shape.currentSquares[i].x = yDistance + currentBlock.shape.xCoordinate
+            currentBlock.shape.currentSquares[i].y = xDistance * -1 + currentBlock.shape.yCoordinate - width
+
+        for j in range(0,4):
+            currentBlock.shape.currentSquares[j].updating(j)
+
+        allShapes.collision()
+        if allShapes.collided:
+            allShapes.collided = False
+            return 'collided'
+
+        for i in range(0,4):
+            if currentRect[i].x < 0 or currentRect[i].x >= playScreenWidth:
+                return 'collided'
+
+
+class Square:
+
+    def __init__(self, x, y):
+        self.y = y
+        self.x = x
+        currentRect.append(pygame.Rect([self.x, self.y], [width, width]))
+
+    def fall(self, direction, i):
+        currentBlock.shape.currentSquares[i].y += (width * direction)
+        currentRect[i] = pygame.Rect([currentBlock.shape.currentSquares[i].x, currentBlock.shape.currentSquares[i].y], [width, width])
+
+    def updating(self,i):
+        currentRect[i].x = currentBlock.shape.currentSquares[i].x
+        currentRect[i].y = currentBlock.shape.currentSquares[i].y
+        currentBlock.shape.assignCentre()
+
+    def checkIfEdge(self):
+        for j in range(0,4):
+            if side == 1:
+                if (currentBlock.shape.currentSquares[j].x + width) == (playScreenWidth):
+                    return True
+            elif side == -1:
+                if (currentBlock.shape.currentSquares[j].x) == 0:
+                    return True
+        return False
+
+    def move(self, side, multiplier, i):
+        self.side = side
+        currentBlock.shape.currentSquares[i].x += width * side * multiplier
+
+
+class ExistingShapes:
+
+    def __init__(self):
+        self.squareList = []
+        self.colours = []
+        self.collision1 = False
+        self.collided = False
+
+    def appending(self):
+        for i in range(0,4):
+            self.squareList.append(currentRect[i])
+        for i in range(0,4):
+            self.colours.append(currentBlock.colour)
+
+    def drawAllShapes(self):
+        for i in range(0, len(self.squareList)):
+            pygame.draw.rect(screen, self.colours[i], self.squareList[i])
+            pygame.draw.rect(screen, 0, self.squareList[i], 1)
+
+    def collision(self):
+
+        for i in range(len(self.squareList)):
+            for j in range(0,4):
+                self.collision1 = (currentRect[j]).colliderect(self.squareList[i])
+                if self.collision1:
+                    self.collided = True
+                    return
+
+    def deletingLines(self):
+        remove = []
+        for i in range(0, playScreenHeight, width):
+            count1 = 0
+            for j in range(len(self.squareList)):
+                if self.squareList[j].y == i:
+                    count1 += 1
+                if count1 == 10:
+                    remove.append(i)
+                    remove = list(set(remove))
+
+        remove2 = []
+        for i in remove:
+            for pos,square in enumerate(self.squareList):
+                if square.y == i:
+                    remove2.append(pos)
+                elif square.y < i:
+                    square.y += width
+        remove2 = list(set(remove2))
+        remove2.sort()
+        for i in remove2[::-1]:
+            self.squareList.pop(i)
+            self.colours.pop(i)
+
+
+class PurpleT:
+    def __init__(self):
+        self.currentSquares = [0,1,2,3]
+        self.currentRect = [0,1,2,3]
+        self.colour = (148,0,211)
+        self.x = 120
+        self.y = 0
+
+        self.currentSquares[0] = Square(self.x, (self.y+width))
+        self.currentSquares[1] = Square((self.x+width), self.y)
+        self.currentSquares[2] = Square((self.x+width), (self.y+width))
+        self.currentSquares[3] = Square((self.x+(2*width)), (self.y+width))
+        self.angle = 0
+
+        self.assignCentre()
+
+    def assignCentre(self):
+        self.xCoordinate = (self.currentSquares[0].x + self.currentSquares[3].x + width)/2
+        self.yCoordinate = (self.currentSquares[0].y + self.currentSquares[3].y + width)/2
+        self.centre = (self.xCoordinate), (self.yCoordinate)
+
+
+class BlueLine:
+    def __init__(self):
+        self.currentSquares = [0,1,2,3]
+        self.currentRect = [0,1,2,3]
+        self.colour = (0,0,205)
+        self.x = 120
+        self.y = 0
+
+        self.currentSquares[0] = Square(self.x, (self.y))
+        self.currentSquares[1] = Square((self.x+width), self.y)
+        self.currentSquares[2] = Square((self.x+(2*width)), (self.y))
+        self.currentSquares[3] = Square((self.x+3*width), (self.y))
+        self.angle = 0
+
+        self.assignCentre()
+
+    def assignCentre(self):
+        y_diff = self.currentSquares[0].y - self.currentSquares[1].y
+        if y_diff == 0:
+            self.xCoordinate = (self.currentSquares[0].x - self.currentSquares[2].x + 2*width)/4
+            self.xCoordinate += self.currentSquares[2].x
+        else:
+            self.xCoordinate = self.currentSquares[0].x + (y_diff+width)/2
+
+
+        x_diff = self.currentSquares[1].x - self.currentSquares[0].x
+        if x_diff == 0:
+            self.yCoordinate = (self.currentSquares[0].y - self.currentSquares[2].y + 2*width)/4
+            self.yCoordinate += self.currentSquares[2].y
+        else:
+            self.yCoordinate = self.currentSquares[0].y + (x_diff+width)/2
+
+        self.centre = (self.xCoordinate), (self.yCoordinate)
+
+
+class YellowSquare:
+
+    def __init__(self):
+        self.currentSquares = [0,1,2,3]
+        self.currentRect = [0,1,2,3]
+        self.colour = (255,255,0)
+        self.x = 160
+        self.y = 0
+
+        self.currentSquares[0] = Square(self.x, (self.y))
+        self.currentSquares[1] = Square((self.x+width), self.y)
+        self.currentSquares[2] = Square((self.x), (self.y+width))
+        self.currentSquares[3] = Square((self.x+width), (self.y+width))
+        self.angle = 0
+
+        self.assignCentre()
+
+    def assignCentre(self):
+        self.xCoordinate = (self.currentSquares[0].x + self.currentSquares[3].x + width)/2
+        self.yCoordinate = (self.currentSquares[0].y + self.currentSquares[3].y + width)/2
+        self.centre = (self.xCoordinate), (self.yCoordinate)
+
+
+
+
+
+        #1 * (distance between y of each)
+        #+ y location
+
+
+class BlueL:
+    def __init__(self):
+        self.currentSquares = [0,1,2,3]
+        self.currentRect = [0,1,2,3]
+        self.colour = (0,0,205)
+        self.x = 120
+        self.y = 0
+
+        self.currentSquares[0] = Square(self.x, (self.y))
+        self.currentSquares[1] = Square((self.x), self.y+width)
+        self.currentSquares[2] = Square((self.x+width), (self.y+width))
+        self.currentSquares[3] = Square((self.x+(2*width)), (self.y+width))
+        self.angle = 0
+
+        self.assignCentre()
+
+    def assignCentre(self):
+        self.xCoordinate = (self.currentSquares[1].x + self.currentSquares[3].x + width)/2
+        self.yCoordinate = (self.currentSquares[1].y + self.currentSquares[3].y + width)/2
+        self.centre = (self.xCoordinate), (self.yCoordinate)
+
+
+class OrangeL:
+    def __init__(self):
+        self.currentSquares = [0,1,2,3]
+        self.currentRect = [0,1,2,3]
+        self.colour = (255,165,0)
+        self.x = 120
+        self.y = 0
+
+        self.currentSquares[0] = Square(self.x+(2*width), (self.y))
+        self.currentSquares[1] = Square((self.x), self.y+width)
+        self.currentSquares[2] = Square((self.x+width), (self.y+width))
+        self.currentSquares[3] = Square((self.x+(2*width)), (self.y+width))
+        self.angle = 0
+
+        self.assignCentre()
+
+    def assignCentre(self):
+        self.xCoordinate = (self.currentSquares[1].x + self.currentSquares[3].x + width)/2
+        self.yCoordinate = (self.currentSquares[1].y + self.currentSquares[3].y + width)/2
+        self.centre = (self.xCoordinate), (self.yCoordinate)
+
+
+class GreenShape:
+    def __init__(self):
+        self.currentSquares = [0,1,2,3]
+        self.currentRect = [0,1,2,3]
+        self.colour = (0,255,0)
+        self.x = 120
+        self.y = 0
+
+        self.currentSquares[0] = Square(self.x, (self.y+width))
+        self.currentSquares[1] = Square((self.x+width), self.y+width)
+        self.currentSquares[2] = Square((self.x+width), (self.y))
+        self.currentSquares[3] = Square((self.x+(2*width)), (self.y))
+        self.angle = 0
+
+        self.assignCentre()
+
+    def assignCentre(self):
+        self.xCoordinate = (self.currentSquares[1].x + 20)
+        self.yCoordinate = (self.currentSquares[1].y + 20)
+        self.centre = (self.xCoordinate), (self.yCoordinate)
+
+
+class RedShape:
+    def __init__(self):
+        self.currentSquares = [0,1,2,3]
+        self.currentRect = [0,1,2,3]
+        self.colour = (255,0,0)
+        self.x = 120
+        self.y = 0
+
+        self.currentSquares[0] = Square(self.x, (self.y))
+        self.currentSquares[1] = Square((self.x+width), self.y)
+        self.currentSquares[2] = Square((self.x+width), (self.y+width))
+        self.currentSquares[3] = Square((self.x+(2*width)), (self.y+width))
+        self.angle = 0
+
+        self.assignCentre()
+
+    def assignCentre(self):
+        self.xCoordinate = self.currentSquares[2].x + 20
+        self.yCoordinate = self.currentSquares[2].y + 20
+        self.centre = (self.xCoordinate), (self.yCoordinate)
+
+
+currentBlock = Block()
+allShapes = ExistingShapes()
+
+def main():
+    global currentBlock, count, spaceDown, downwardsKey, sideKeyDown, allShapes, currentRect, side
+
+    for i in range(0,4):
+        currentBlock.shape.currentSquares[i].updating(i)
+
+    screen.fill(white)
+    for rectangle in screenRect:
+        pygame.draw.rect(screen, 0, rectangle)
+    allShapes.drawAllShapes()
+    currentBlock.drawing()
+
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            exit()
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                side = 1
+                sideKeyDown = True
+
+            elif event.key == pygame.K_LEFT:
+                side = -1
+                sideKeyDown = True
+
+            elif event.key == pygame.K_DOWN:
+                downwardsKey = True
+
+            elif event.key == pygame.K_UP:
+                rotate = currentBlock.rotate()
+                if rotate == 'collided':
+                    currentBlock.rotateClockwise()
+
+            elif event.key == pygame.K_SPACE:
+                spaceDown = True
+
+            elif event.key == pygame.K_z:
+                rotate = currentBlock.rotateClockwise()
+                if rotate == 'collided':
+                    currentBlock.rotate()
+
+
+        if event.type == pygame.KEYUP:
+            sideKeyDown = False
+            downwardsKey = False
+
+    if int(count%40) == 0:
+        if sideKeyDown:
+            if currentBlock.shape.currentSquares[1].checkIfEdge() == False:
+                for i in range(0,4):
+                    currentBlock.shape.currentSquares[i].move(side, 1, i)
+            for i in range(0,4):
+                currentBlock.shape.currentSquares[i].updating(i)
+            allShapes.collision()
+            if allShapes.collided:
+                for i in range(0,4):
+                    currentBlock.shape.currentSquares[i].move(side, -1, i)
+            allShapes.collided = False
+
+    if int(count%30) == 0:
+        if downwardsKey:
+            currentBlock.falling(1)
+            #currentBlock.shape.draw()
+            allShapes.collision()
+            if allShapes.collided:
+                currentBlock.falling(-1)
+
+    if int(count%400) == 0 and not(allShapes.collided):
+        currentBlock.falling(1)
+        allShapes.collision()
+        if allShapes.collided:
+            currentBlock.falling(-1)
+
+    if spaceDown:
+        currentBlock.falling(1)
+        allShapes.collision()
+        if allShapes.collided:
+            currentBlock.falling(-1)
+            spaceDown = False
+
+    for i in range(0,4):
+        if (currentBlock.shape.currentSquares[i].y + width) == playScreenHeight:
+            allShapes.collided = True
+
+
+    if allShapes.collided:
+        print('here')
+        spaceDown = False
+        allShapes.appending()
+
+        allShapes.collided = False
+        allShapes.deletingLines()
+        currentRect = []
+        currentBlock = Block()
+
+    count+=1
+    clock.tick(1000)
+    pygame.display.flip()
+
+
+while True:
+    main()
